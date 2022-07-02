db753c6ecfdecb901dd7d6a081f1112808d1aaf7
Stuti-jain <60813293+Stuti-jain@users.noreply.github.com>
2022-07-02 16:19:14 +0530

    Delete Restaurants directory

--- Restaurants/src/main/java/com/example/demo/RestaurantNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo;
-
-public class RestaurantNotFoundException extends RuntimeException {
-
-    RestaurantNotFoundException(Long id) {
-        super("This restaurant does not exist " + id);
-    }
-}
--- Restaurants/src/main/java/com/example/demo/RestaurantRepository.java
+++
@@ -1,5 +0,0 @@
-package com.example.demo;
-import org.springframework.data.jpa.repository.JpaRepository;
-public interface RestaurantRepository extends JpaRepository<Restaurant, Long>{
-
-}
